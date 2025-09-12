ddceec20aa1848b5119d374b26f79205c1df394d
MathCat975 <120200839+MathCat975@users.noreply.github.com>
2025-09-12 16:18:26 +0200

    Refactor Potion struct and add predefined potions
    Changed the Size field in the Potion struct from string to int. Added predefined Poison and Cure potions, and introduced an AllPotions map for easy access to these potions.

--- src/pkg/structures/potions.go
+++ src/pkg/structures/potions.go
@@ -1,7 +1,25 @@
 package structures
 
 type Potion struct {
-	Size string
+	Size int
 	Type string
 	Name string
+}
+
+var (
+	Poison = Potion{
+		Size: 1,
+		Type: "Poison",
+		Name: "Poison Potion",
+	}
+	Cure = Potion{
+		Size: 1,
+		Type: "Cure",
+		Name: "Cure Potion",
+	}
+)
+
+var AllPotions = map[string]Potion{
+	"Poison": Poison,
+	"Cure":   Cure,
 }
