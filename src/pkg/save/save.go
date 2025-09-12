a291c422e2b0ca5f462bc938c76607e30a650239
Sysy's <sysys.iwi@gmail.com>
2025-09-12 16:39:06 +0200

    Inline SaveToWriter and LoadFromReader logic
    Removed the SaveToWriter and LoadFromReader helper functions and inlined their logic directly into SaveAny and LoadAny. This simplifies the code by reducing indirection and removes unnecessary function wrappers.

--- src/pkg/save/save.go
+++ src/pkg/save/save.go
@@ -3,32 +3,8 @@ package save
 import (
 	"encoding/json"
 	"errors"
-	"io"
 	"os"
 )
-
-func SaveToWriter(obj interface{}, w io.Writer) error {
-	if obj == nil {
-		return errors.New("object to save cannot be nil")
-	}
-	if w == nil {
-		return errors.New("writer cannot be nil")
-	}
-	encoder := json.NewEncoder(w)
-	encoder.SetIndent("", "  ")
-	return encoder.Encode(obj)
-}
-
-func LoadFromReader(obj interface{}, r io.Reader) error {
-	if obj == nil {
-		return errors.New("object to load cannot be nil")
-	}
-	if r == nil {
-		return errors.New("reader cannot be nil")
-	}
-	decoder := json.NewDecoder(r)
-	return decoder.Decode(obj)
-}
 
 func isSaveFolderExists(saveId string) bool {
 	_, err := os.Stat("saves/" + saveId)
@@ -58,7 +34,12 @@ func SaveAny(saveId, fileName string, obj interface{}) error {
 	}
 	defer jsonFile.Close()
 
-	return SaveToWriter(obj, jsonFile)
+	if obj == nil {
+		return errors.New("object to save cannot be nil")
+	}
+	encoder := json.NewEncoder(jsonFile)
+	encoder.SetIndent("", "  ")
+	return encoder.Encode(obj)
 }
 
 func LoadAny(saveId, fileName string, obj interface{}) error {
@@ -73,5 +54,9 @@ func LoadAny(saveId, fileName string, obj interface{}) error {
 	}
 	defer jsonFile.Close()
 
-	return LoadFromReader(obj, jsonFile)
+	if obj == nil {
+		return errors.New("object to load cannot be nil")
+	}
+	decoder := json.NewDecoder(jsonFile)
+	return decoder.Decode(obj)
 }
